b109ccc935644e61ccf5f6399b07648138cd26be
rostam <zaalire2@gmail.com>
2025-08-09 14:03:36 +0330

    build main app and others

--- TaskManagerAPI/TaskManagerAPI/settings.py
+++ TaskManagerAPI/TaskManagerAPI/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+
+    'task'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'TaskManagerAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taskmanager_db',
+        'USER' : 'taskuser',
+        'PASSWORD' : 'Alirez@1381',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432' 
     }
 }
 
